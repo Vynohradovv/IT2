8bfc1a1d8b2edeca68e7de5a3ff6ab9121cc9647
Anton <233302@edu.p.lodz.pl>
2020-06-17 17:11:07 +0200

    zadanie 29.05

---
+++ Complex_Numbers/Complex_Numbers/Complex_Numbers.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+template< class T >
+class Complex {
+
+    template<typename U>
+    friend std::ostream& operator<< (std::ostream&, const Complex<U>&);
+
+public:
+    explicit Complex(const T& Re = 0, const T& Im = 0);
+
+    Complex operator+(const Complex&) const;
+    Complex operator-(const Complex&) const;
+
+private:
+    T Re;
+    T Im;
+};
+
+template< class T >
+std::ostream& operator << (std::ostream& out, const Complex< T >& obj)
+{
+    return out << "Re = " << obj.Re << "  Im = " << obj.Im;
+}
+
+template <class T>
+Complex<T>::Complex(const T& Re, const T& Im)
+{
+    this->Re = Re;
+    this->Im = Im;
+}
+
+template <class T>
+Complex<T> Complex<T>::operator+(const Complex<T>& other) const
+{
+    return Complex<T>(Re + other.Re, Im + other.Im);
+}
+
+template< class T >
+Complex<T> Complex<T>::operator-(const Complex<T>& other) const
+{
+    return Complex<T>(Re - other.Re, Im - other.Im);
+}
+
+int main()
+{
+    Complex< int > A(12, 34), B(12, -24);
+
+    std::cout << A + B << std::endl << A - B << std::endl;
+    std::cin.get();
+
+    return 0;
+}
